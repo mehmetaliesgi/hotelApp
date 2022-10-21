1b4a5e24a67f5ec83240cb74dd3541ace485053a
Mehmet Ali EŞGİ <mehmetaliesgi@gmail.com>
2022-10-22 00:55:43 +0300

    Admin dashboard theme integration finished

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,12 @@
+class AdminController < ApplicationController
+    layout "admin"
+    def index
+
+    end
+    
+    def room
+
+    end
+
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+
+  root "admin#index"
+
+  get 'room', to: 'admin#room'
+end
