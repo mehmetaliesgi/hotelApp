56b3ba67d7c8156e4b81c8f4ed5b2bbc68066846
Mehmet Ali EŞGİ <mehmetaliesgi@gmail.com>
2022-11-26 00:14:32 +0300

    room_color method has been created in admin_helper.

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,9 +1,17 @@
 module AdminHelper
-    def short_descripton(description)
-        if description.size < 10
-            return description
-        else
-            return description[0..10].strip + "..."
-        end
+  def short_descripton(description)
+    if description.size < 10
+      return description
+    else
+      return description[0..10].strip + "..."
     end
+  end
+
+  def room_color(room)
+    if room.status == 'Müsait'
+      "card bg-success text-white shadow"                                                
+    else
+      "card bg-danger text-white shadow"   
+    end                         
+  end
 end
