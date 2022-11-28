e03001fe2e8c841cace336f4e23c6928fed34248
Mehmet Ali EŞGİ <mehmetaliesgi@gmail.com>
2022-11-28 21:59:51 +0300

    Service routes fixed and _error file created for services.

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,12 +1,5 @@
 module AdminHelper
-  def short_descripton(description)
-    if description.size < 10
-      return description
-    else
-      return description[0..10].strip + "..."
-    end
-  end
-
+  
   def room_color(room)
     if room.status == 'Müsait'
       "card bg-success text-white shadow"                                                
--- config/routes.rb
+++ config/routes.rb
@@ -10,13 +10,6 @@ Rails.application.routes.draw do
   resources :rooms
 
   #service routes
-  get 'services', to: 'services#index'
-
-  get 'service/:id/edit', to: 'services#edit', as: :edit_service
-  patch 'service/:id', to: 'services#update'
-
-  get 'service/new', to: 'services#new'
-  post 'service/create', to: 'services#create'
-
-  delete 'service/:id', to: 'services#destroy', as: :delete_service
+  resources :services
+  
 end
