70d518fa6966ebe9f7f66c384e5e64c645385899
Mehmet Ali EŞGİ <mehmetaliesgi@gmail.com>
2022-11-29 23:24:51 +0300

    The validates of the User model has been added.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    validates :name, :surname, :email, :tckn, :telefon, presence: true
+    validates :email, :tckn, uniqueness: true
+    validates :name, :surname, :email, length: { minimum: 2, maximum: 50}
+    validates :tckn, :telefon, :telefon2, numericality: { only_integer: true }
 end
