78899e32f40d04e6849c17d88e5eea2cdf0c92ad
Mehmet Ali EŞGİ <mehmetaliesgi@gmail.com>
2022-12-05 01:41:17 +0300

    Added Admin login controls lines.

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -40,12 +40,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
    def configure_sign_up_params
-     devise_parameter_sanitizer.permit(:sign_up, keys: [:tckn, :name, :surname, :phone, :phone2])
+     devise_parameter_sanitizer.permit(:sign_up, keys: [:tckn, :name, :surname, :phone, :phone2, :role])
    end
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-     devise_parameter_sanitizer.permit(:account_update, keys: [:tckn, :name, :surname, :phone, :phone2])
+     devise_parameter_sanitizer.permit(:account_update, keys: [:tckn, :name, :surname, :phone, :phone2, :role])
   end
 
   # The path used after sign up.
